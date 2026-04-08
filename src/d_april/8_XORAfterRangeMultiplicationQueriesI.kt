3df67f4af6ad09f81080fb36921fa7dd209f4d9c
charlie <crohitp9@gmail.com>
2026-04-08 10:51:33 +0530

    April : 8. XOR After Range Multiplication Queries I.

---
+++ src/d_april/8_XORAfterRangeMultiplicationQueriesI.kt
@@ -0,0 +1,31 @@
+package d_april
+
+fun main() {
+
+    val testCases = listOf(
+        Pair(
+            intArrayOf(1, 1, 1),
+            arrayOf(
+                intArrayOf(0, 2, 1, 4)
+            )
+        )
+    )
+
+    testCases.forEach { (nums, queries) ->
+        println("Result ==> ${xorAfterQueries(nums, queries)}")
+    }
+
+}
+
+// TC - O(q * n) : SC - O(1)
+fun xorAfterQueries(nums: IntArray, queries: Array<IntArray>): Int {
+    val mod = 1000000007
+    for ((l, r, k, v) in queries) {
+        for (i in l..r step k) {
+            nums[i] = ((nums[i].toLong() * v) % mod).toInt()
+        }
+    }
+    var res = 0
+    for (num in nums) res = res xor num
+    return res
+}
