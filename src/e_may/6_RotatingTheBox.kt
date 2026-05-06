6577e70d02b547309e9bdb6a657be1585ed8b754
charlie <crohitp9@gmail.com>
2026-05-06 13:55:44 +0530

    May : 6. Rotate The Box.

---
+++ src/e_may/6_RotatingTheBox.kt
@@ -0,0 +1,43 @@
+package e_may
+
+fun main() {
+
+    val testCases = listOf(
+        arrayOf(charArrayOf('#', '.', '#'))
+    )
+
+    testCases.forEach { boxGrid ->
+        println("Result ==> ${rotateTheBox(boxGrid)}")
+    }
+
+}
+
+// TC - O(m * n) :: SC - O(n * m)
+fun rotateTheBox(boxGrid: Array<CharArray>): Array<CharArray> {
+    val m = boxGrid.size // row -> column
+    val n = boxGrid[0].size // column -> row
+
+    val mat = Array(n) { CharArray(m) { '.' } }
+
+    for (r in n - 1 downTo 0) {
+        for (c in m - 1 downTo 0) {
+            mat[r][c] = boxGrid[m - 1 - c][r]
+        }
+    }
+
+    for (c in m - 1 downTo 0) {
+        var r = n - 1
+        for (l in r downTo 0) {
+            if (mat[l][c] == '#') {
+                mat[l][c] = '.'
+                mat[r--][c] = '#'
+            }
+
+            if (mat[l][c] == '*') {
+                r = l - 1
+            }
+        }
+    }
+
+    return mat
+}
