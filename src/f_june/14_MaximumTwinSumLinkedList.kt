2069edaaa04c991ff68917a044477b18d566c4c9
charlie <crohitp9@gmail.com>
2026-06-15 13:47:52 +0530

    14. Maximum Twin Sum of a Linked List.

--- src/f_june/14_MaximumTwinSumLinkedList.kt
+++ src/f_june/14_MaximumTwinSumLinkedList.kt
@@ -1,10 +1,53 @@
 package f_june
 
 import z_data_types.ListNode
+import z_data_types.createLinkedList
 
 fun main() {
 
+    val testCases = listOf(
+        listOf(5, 4, 2, 1),
+        listOf(4, 2, 2, 3),
+        listOf(1, 100000)
+    )
 
+    testCases.forEach { list ->
+        val head = createLinkedList(list)
+        println("Result ==> ${pairSum(head)}")
+    }
+
+}
+
+// TC - O(n) :: SC - O(n)
+fun pairSum(head: ListNode?): Int {
+    var slow: ListNode? = head
+    var fast: ListNode? = head
+
+    while (fast != null && fast.next != null) {
+        fast = fast.next?.next
+        slow = slow?.next
+    }
+
+    var rightHead: ListNode? = null
+    while (slow != null) {
+        val temp = slow.next
+        slow.next = rightHead
+        rightHead = slow
+        slow = temp
+    }
+
+    var leftHead = head
+    var maxPair = 0
+    while (rightHead != null && leftHead != null) {
+        maxPair = maxOf(
+            maxPair,
+            rightHead.`val` + leftHead.`val`
+        )
+        leftHead = leftHead.next
+        rightHead = rightHead.next
+    }
+
+    return maxPair
 }
 
 // TC - O(n) :: SC - O(n)
